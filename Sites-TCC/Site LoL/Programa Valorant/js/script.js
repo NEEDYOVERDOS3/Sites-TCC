98d6880be2b88469a291a072dc34f720960c9b77
Kadu Ribeiro <75531383+KaduRibeiroC@users.noreply.github.com>
2024-03-18 19:50:10 -0300

    Add files via upload

---
+++ Sites-TCC/Site LoL/Programa Valorant/js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const wrapper = document.querySelector('.imagem-mostruario');
+    const curtain = document.querySelector('.cortina-efx');
+  
+    wrapper.addEventListener('mouseover', function(){
+      curtain.style.width = '100%';
+    });
+  
+    wrapper.addEventListener('mouseout', function(){
+      curtain.style.width = '0';
+    });
+  
+  });
+  
+  document.addEventListener('DOMContentLoaded', function(){
+    const wrapper = document.querySelector('.imagem-mostruario2');
+    const curtain = document.querySelector('.cortina-efx2');
+  
+    wrapper.addEventListener('mouseover', function(){
+      curtain.style.width = '100%';
+    });
+  
+    wrapper.addEventListener('mouseout', function(){
+      curtain.style.width = '0';
+    });
+  
+  });
+  
+  document.addEventListener('DOMContentLoaded', function(){
+    const wrapper = document.querySelector('.imagem-mostruario3');
+    const curtain = document.querySelector('.cortina-efx3');
+  
+    wrapper.addEventListener('mouseover', function(){
+      curtain.style.width = '100%';
+    });
+  
+    wrapper.addEventListener('mouseout', function(){
+      curtain.style.width = '0';
+    });
+  
+  });
