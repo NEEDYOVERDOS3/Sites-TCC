96d420321bf86089f8657337490fb58d42ab02b4
Needy Overdose <162506710+NEEDYOVERDOS3@users.noreply.github.com>
2024-03-27 11:03:24 -0300

    Add files via upload

---
+++ Sites-TCC/Sites-TCC/Site LoL/Cadastro/script/script.js
@@ -0,0 +1,22 @@
+var videoPaths = [
+    'img/video/PRESTIGE STAR GUARDIAN SYNDRA 1080p.mp4',
+    'img/video/star-guardian-xayah-and-rakan-league-of-legends-moewalls-com.mp4',
+    'img/video/Battle Bunny Miss Fortune Animated Login Screen.mp4',
+    'img/video/arcane-jinx-league-of-legends-moewalls-com.mp4',
+    'img/video/Ahri - Spirit Blossom Animated Wallpaper 1080p (HD).mp4',
+    'img/video/MORGANA NEMESIS ESTELAR.mp4'
+  ];
+  
+  
+  function getRandomIndex(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  function changeBackgroundVideo() {
+    var videoIndex = getRandomIndex(videoPaths.length);
+    var video = document.getElementById('bg-video');
+    video.src = videoPaths[videoIndex];
+    video.load();
+  }
+  
+  window.onload = changeBackgroundVideo;
